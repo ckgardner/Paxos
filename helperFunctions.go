e5f30b7742748f4d52c56d6ea67feef2636efc03
cade.gardner <d00299297@dmail.dixie.edu>
2020-03-26 20:09:09 -0600

    Terrible shot at Propose

--- helperFunctions.go
+++ helperFunctions.go
@@ -5,7 +5,92 @@ import(
 	"strings"
 	"fmt"
 	"strconv"
+	"net/rpc"
 )
+// Propose function
+func(replica *Replica) Propose(req *DecideRequest, item Slot) error{
+	finished := false
+	for !finished{
+		fmt.Println("Starting Proposal Loop")
+		fmt.Println("Highest slot:", replica.Slots[req.Slot].Promise)
+		var data Slot
+		data.Decided = replica.Slots[req.Slot].Decided
+		item.Decided = replica.Slots[req.Slot].Decided
+		replica.Slots[req.Slot].Accepted.Command = item.Command.Command
+		seen := -1
+		completed := false
+		for !completed{
+			data.Command.Command = replica.Slots[req.Slot].Accepted.Command
+			item.HighestN = seen + 1
+			data.HighestN = seen + 1
+			replica.Slots[req.Slot].Accepted.SequenceN = seen + 1
+			totalOk := 0
+			totalNot := 0
+			for _, add := range replica.Cell{
+				time.Sleep(1000 * time.Millisecond)
+				var prepOk Slot
+				fmt.Println("Asking", add)
+				if err := call(add, "Node.Prepare", data, &prepOk); err != nil{
+					log.Printf("Bad prepare from %v", add)
+				} else{
+					if prepOk.Decided{
+						totalOk ++
+					}else{
+						totalNot ++
+						fmt.Printf("%v rejected", add)
+						if prepOk.HighestN > seen{
+							seen = prepOk.HighestN
+						}
+						if len(prepOk.Command.Command) > 0{
+							replica.Slots[req.Slot].Accepted.Command = prepOk.Command.Command
+						}
+					}
+				}
+				if totalOk > len(replica.Cell)/2 || totalNot > len(replica.Cell)/2{
+					break
+				}
+			}
+			if totalNot > len(replica.Cell)/2{
+				fmt.Println("Majority declined... retry")
+				time.Sleep(1000 * time.Millisecond)
+				continue
+			}
+			fmt.Println("Received a prepare majority. Accepting..")
+			var prepOk Slot
+			totalOk = 0
+			totalNot = 0
+			for _, add := range replica.Cell{
+				time.Sleep(1000 * time.Millisecond)
+				if err := call(add, "Node.Accept", data, &prepOk); err != nil{
+					log.Printf("Bad decide call from %v", add)
+				}else{
+					if prepOk.Decided {
+						totalOk ++
+					} else{
+						fmt.Printf("%v rejected", add)
+						totalNot ++
+					}
+				}
+			}
+			if totalOk > len(replica.Cell)/2{
+				fmt.Println("Got an accept majority. Deciding..")
+				for _, add := range replica.Cell{
+					if err := call(add, "Node.Decide", data, struct{}{}); err != nil{
+						log.Printf("bad decide call from %v", add)
+					}
+				}
+				if data.Command.Command == item.Command.Command{
+					completed = true
+				}
+			} else{
+				time.Sleep(1000 * time.Millisecond)
+			}
+			completed = true
+		}
+		replica.Slots[req.Slot].HighestN = -1
+	}
+	return nil
+}
 
 // Prepare is not an RPC
 func(replica *Replica) Prepare(req *PrepareRequest, res *PrepareResponse){
@@ -121,19 +206,19 @@ func(replica *Replica) Decide(req *DecideRequest, res *Nothing){
 	time.Sleep(1000 * time.Millisecond)
 }
 
-// func call(address string, method string, request interface{}, reply interface{}) error {
-// 	client, err := rpc.DialHTTP("tcp", address)
-// 	if err != nil {
-// 		log.Printf("rpc.DialHTTP: %v", err)
-// 		return err
-// 	}
+func call(address string, method string, request interface{}, reply interface{}) error {
+	client, err := rpc.DialHTTP("tcp", address)
+	if err != nil {
+		log.Printf("rpc.DialHTTP: %v", err)
+		return err
+	}
 
-// 	defer client.Close()
+	defer client.Close()
 
-// 	if err = client.Call(method, request, reply); err != nil{
-// 		log.Printf("client.Call %s: %v", method, err)
-// 		return err
-// 	}
+	if err = client.Call(method, request, reply); err != nil{
+		log.Printf("client.Call %s: %v", method, err)
+		return err
+	}
 
-// 	return nil
-// }
+	return nil
+}
